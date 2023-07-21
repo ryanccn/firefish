4dbd7e5b047c8d24e4d0e607e1c3ae683445b038
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-21 14:54:58 +0800

    fix: add author and skip if unchanged [skip ci]

--- update.sh
+++ update.sh
@@ -11,6 +11,7 @@ log_prefix="$ansi_dim> $ansi_reset"
 [ -d src-update ] && rm -rf src-update
 
 clone_url="https://gitlab.prometheus.systems/firefish/firefish.git"
+old_rev="$(cat rev.txt)"
 
 echo -e "${log_prefix}Cloning ${ansi_cyan}firefish#beta${ansi_reset}..."
 
@@ -22,7 +23,13 @@ cd ..
 echo "$rev" > rev.txt
 rm -rf src-update
 
-echo -e "${log_prefix}Updated pinned revision to ${ansi_cyan}$rev${ansi_reset}"
+if [ "$old_rev" != "$rev" ]; then
+  echo -e "${log_prefix}Updated pinned revision to ${ansi_cyan}$rev${ansi_reset}"
+  echo "updated=true" >> "$GITHUB_OUTPUT"
+else
+  echo -e "${log_prefix}Pinned revision ${ansi_cyan}$old_rev${ansi_reset} is up to date"
+  echo "updated=false" >> "$GITHUB_OUTPUT"
+fi
 
 if [ -n "$GITHUB_OUTPUT" ]; then
   echo "rev=$rev" >> "$GITHUB_OUTPUT"
