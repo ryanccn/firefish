89a70022466824899113a08589b0f992847f5737
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-07-21 20:57:12 +0800

    feat: fetch from GitLab API (way faster)

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,31 +8,31 @@ log_prefix="$ansi_dim> $ansi_reset"
 
 [ -d src-update ] && rm -rf src-update
 
-clone_url="https://gitlab.prometheus.systems/firefish/firefish.git"
 old_rev="$(cat rev.txt)"
 
-echo -e "${log_prefix}Cloning ${ansi_cyan}firefish#beta${ansi_reset}..."
+echo -e "${log_prefix}Fetching ${ansi_cyan}latest revision${ansi_reset} from GitLab API..."
 
-git clone "$clone_url" --depth 1 --branch beta --single-branch src-update
-cd src-update
+commit_data="$(curl -fsSL "https://gitlab.prometheus.systems/api/v4/projects/30/repository/commits?ref_name=beta" | jq '.[0]')"
 
-rev="$(git rev-parse HEAD)"
-cd ..
+rev="$(echo "$commit_data" | jq -r '.id')"
+short_rev="$(echo "$commit_data" | jq -r '.short_id')"
+message="$(echo "$commit_data" | jq -r '.title')"
+
 echo "$rev" > rev.txt
-rm -rf src-update
 
 if [ "$old_rev" != "$rev" ]; then
-  echo -e "${log_prefix}Updated pinned revision to ${ansi_cyan}$rev${ansi_reset}"
+  echo -e "${log_prefix}Updated pinned revision to $ansi_cyan$short_rev$ansi_reset ($message)"
   set_output updated "true"
 else
-  echo -e "${log_prefix}Pinned revision ${ansi_cyan}$old_rev${ansi_reset} is up to date"
+  echo -e "${log_prefix}Pinned revision $ansi_cyan$old_rev$ansi_reset ($message) is up to date"
   set_output updated "false"
 fi
 
+pr_title="feat: update Firefish to \`$short_rev\`"
 pr_body="$(cat << EOF
-Updates Firefish beta to [\`$rev\`](https://gitlab.prometheus.systems/firefish/firefish/-/commit/$rev).
+Updates Firefish beta to [$message (\`$short_rev\`)](https://gitlab.prometheus.systems/firefish/firefish/-/commit/$rev).
 EOF
 )"
 
-set_output rev "$rev"
+set_output pr_title "$pr_title"
 set_output pr_body "$pr_body"
